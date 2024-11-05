6ce724708893f3c0bff3589f85bf4eb879098db2
lehungio <me@lehungio.com>
2024-11-05 21:00:42 +0900

    tweak: nodejs version

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,26 @@
 'use strict'
 
-module.exports = { extends: ['@commitlint/config-conventional'] }
+// https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional#type-enum
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+	'type-enum': [
+	  2,
+	  'always',
+	  [
+		'build',
+		'chore',
+		'ci',
+		'docs',
+		'feat',
+		'fix',
+		'perf',
+		'refactor',
+		'revert',
+		'style',
+		'test',
+    'tweak'
+	  ]
+	]
+  }
+}
